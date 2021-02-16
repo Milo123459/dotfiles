2a1ce0be7a037f1fa3d36a18f8c715d23afbb2d9
Milo <50248166+Milo123459@users.noreply.github.com>
2021-02-16 14:35:41 +0000

    Update powershell.ps1

--- powershell.ps1
+++ powershell.ps1
@@ -2,5 +2,3 @@ clear
 Invoke-Expression (&starship init powershell)
 $host.UI.RawUI.WindowTitle = "PowerShell"
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
-Set-PSReadlineKeyHandler -Key LeftArrow -Function HistorySearchBackward
-Set-PSReadlineKeyHandler -Key RightArrow -Function HistorySearchForward
