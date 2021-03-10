95358be078a2a0ee4c3fc71769bdb2b8602953af
Milo <50248166+Milo123459@users.noreply.github.com>
2021-03-10 13:30:53 +0000

    Update powershell.ps1

--- powershell.ps1
+++ powershell.ps1
@@ -1,5 +1,4 @@
 clear
-Invoke-Expression (&scoop-search --hook)
 Invoke-Expression (&starship init powershell)
 $host.UI.RawUI.WindowTitle = "PowerShell"
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
