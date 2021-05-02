2b47ed26f338c494cb574342cd4dd0bf67d0e7ac
Milo <50248166+Milo123459@users.noreply.github.com>
2021-05-02 17:03:42 +0100

    Update powershell.ps1

--- powershell.ps1
+++ powershell.ps1
@@ -3,3 +3,15 @@ Invoke-Expression (&starship init powershell)
 $host.UI.RawUI.WindowTitle = "PowerShell"
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 Remove-Item alias:curl
+function oa() {
+    alacritty --working-directory $PWD
+}
+function cc() {
+    cargo clippy --workspace --locked --all-targets --all-features -- -D clippy::all
+}
+function cr() {
+    cargo run --
+}
+function cf() {
+    cargo fmt
+}
