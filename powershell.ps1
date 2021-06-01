d0bc640e774945c25a0ee7b656336a4490f3e158
Milo <50248166+Milo123459@users.noreply.github.com>
2021-06-01 12:52:04 +0100

    Update powershell.ps1

--- powershell.ps1
+++ powershell.ps1
@@ -3,3 +3,4 @@ Invoke-Expression (&starship init powershell)
 $host.UI.RawUI.WindowTitle = "PowerShell"
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 Remove-Item alias:curl
+Remove-Item alias:wget
