d2eb7c2bb7fa4a1952ac952fd4cf6cf5bb7cb04f
Milo <50248166+Milo123459@users.noreply.github.com>
2021-06-15 19:24:56 +0100

    Update powershell.ps1

--- powershell.ps1
+++ powershell.ps1
@@ -4,3 +4,7 @@ $host.UI.RawUI.WindowTitle = "PowerShell"
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 Remove-Item alias:curl
 Remove-Item alias:wget
+Invoke-Expression (& {
+    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
+    (zoxide init --hook $hook powershell) -join "`n"
+})
