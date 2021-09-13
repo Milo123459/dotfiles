8ccdbff1b751182c6a05a3a7ff55da14cd309635
Milo <50248166+Milo123459@users.noreply.github.com>
2021-09-13 23:41:11 +0100

    Update powershell

--- powershell.ps1
+++ powershell.ps1
@@ -1,4 +1,4 @@
-clear
+Clear-Host
 Invoke-Expression (&starship init powershell)
 $host.UI.RawUI.WindowTitle = "PowerShell"
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
@@ -8,3 +8,7 @@ Invoke-Expression (& {
     $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
     (zoxide init --hook $hook powershell) -join "`n"
 })
+function tokio {
+    cd ~/Desktop/GitHub/console
+    cargo run --release
+}
