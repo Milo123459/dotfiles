fc25cefb9b0f03248474be986c27fed10207b98a
Milo <50248166+Milo123459@users.noreply.github.com>
2021-10-26 17:55:09 +0100

    a

--- powershell.ps1
+++ powershell.ps1
@@ -16,4 +16,3 @@ $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
-fnm env --use-on-cd | Out-String | Invoke-Expression
