055de84b303c024884645c352926ef54266e376f
Milo <50248166+Milo123459@users.noreply.github.com>
2021-11-01 18:35:33 +0000

    y

--- powershell.ps1
+++ powershell.ps1
@@ -16,3 +16,5 @@ $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
+Remove-Item alias:cd
+Set-Alias -Name cd -Value z
