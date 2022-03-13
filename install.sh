4f15951d7dfbae7080e6949b5e96af35bf58021c
Milo <50248166+Milo123459@users.noreply.github.com>
2022-03-13 22:57:07 +0000

    revert the comment

--- install.sh
+++ install.sh
@@ -11,5 +11,5 @@ curl -sS https://starship.rs/install.sh | sh -s -- -y
 echo "copying starship config"
 cp $DIR/starship.toml ~/.config/starship.toml
 
-# echo "installing glitter"
-# curl -fsSL https://raw.githubusercontent.com/Milo123459/glitter/master/install.sh | sh -s -- -y
+echo "installing glitter"
+curl -fsSL https://raw.githubusercontent.com/Milo123459/glitter/master/install.sh | sh -s -- -y
