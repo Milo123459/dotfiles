fc12c7d9b754029a81bd88f182552ec9d8837d2c
Milo <50248166+Milo123459@users.noreply.github.com>
2022-04-14 14:33:49 +0100

    try fix even more things

--- install.sh
+++ install.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
-echo "copy bashrc"
-cp .bashrc $HOME/.bashrc
-
 echo "installing starship"
 curl -sS https://starship.rs/install.sh | sh -s -- -y
 
 echo "copying starship config"
-cp starship.toml $HOME/.config/starship.toml
+cp ./starship.toml $HOME/.config/starship.toml
