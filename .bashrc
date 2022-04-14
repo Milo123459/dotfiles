69ceca6741bb9ab0b39a7b4287af985f8c4b2d80
Milo <50248166+Milo123459@users.noreply.github.com>
2022-04-14 14:29:25 +0100

    update stuff

--- .bashrc
+++ .bashrc
@@ -1,1 +1,7 @@
 eval "$(starship init bash)"
+
+nightly() {
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    rustup toolchain add nightly
+    rustup default nightly
+}
--- install.sh
+++ install.sh
@@ -3,13 +3,10 @@
 DIR=$HOME
 
 echo "copy bashrc"
-cp $DIR/.bashrc $HOME/.bashrc
+cp .bashrc $HOME/.bashrc
 
 echo "installing starship"
 curl -sS https://starship.rs/install.sh | sh -s -- -y
 
 echo "copying starship config"
 cp $DIR/starship.toml ~/.config/starship.toml
-
-echo "installing glitter"
-curl -fsSL https://raw.githubusercontent.com/Milo123459/glitter/master/install.sh | sh -s -- -y
