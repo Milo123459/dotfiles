81d53bc7ac20f6bdf09e51c7ff6ea5fb8630cc75
Milo <50248166+Milo123459@users.noreply.github.com>
2022-04-14 14:36:25 +0100

    updates again

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+echo "copy bashrc"
+cp $DIR/.bashrc $HOME/.bashrc
+
 echo "installing starship"
 curl -sS https://starship.rs/install.sh | sh -s -- -y
 
 echo "copying starship config"
-cp ./starship.toml $HOME/.config/starship.toml
+cp $DIR/starship.toml $HOME/.config/starship.toml
