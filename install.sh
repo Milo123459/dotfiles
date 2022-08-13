6f94457ecfea3ad85110dfe060b159f1bb9c1da2
Milo <50248166+Milo123459@users.noreply.github.com>
2022-08-13 22:23:08 +0100

    glita

--- install.sh
+++ install.sh
@@ -10,3 +10,6 @@ curl -sS https://starship.rs/install.sh | sh -s -- -y
 
 echo "copying starship config"
 cp $DIR/starship.toml $HOME/.config/starship.toml
+
+echo "installing glitter"
+curl -fsSL https://raw.githubusercontent.com/Milo123459/glitter/master/install.sh | sh -s -- -y 
