ca9f9910d707628d93e1009591947880317f2a70
Milo <50248166+Milo123459@users.noreply.github.com>
2022-08-14 21:27:57 +0100

    fix dotfiles

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ echo "installing starship"
 curl -sS https://starship.rs/install.sh | sh -s -- -y
 
 echo "copying starship config"
-cp $DIR/starship.toml $HOME/.config/starship.toml
+cp starship.toml $HOME/.config/starship.toml
 
 echo "installing glitter"
 curl -fsSL https://raw.githubusercontent.com/Milo123459/glitter/master/install.sh | bash -s -- -y 
