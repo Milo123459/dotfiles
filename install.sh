5dbd57d2dbefa56d2a1d7ed98fe108326a8dbf3e
Milo <50248166+Milo123459@users.noreply.github.com>
2022-08-15 22:54:51 +0100

    update rust stuff

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ cp .bashrc $HOME/.bashrc
 cp starship.toml $HOME/.config/starship.toml
 curl -sS https://starship.rs/install.sh | sh -s -- -y
 curl -fsSL https://raw.githubusercontent.com/Milo123459/glitter/master/install.sh | bash -s -- -y 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain stable
