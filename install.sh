08c207a15b299db344adbd97ba73ab1cf3170db0
Milo <50248166+Milo123459@users.noreply.github.com>
2022-09-26 18:30:38 +0100

    install gh if it isn't in path

--- install.sh
+++ install.sh
@@ -7,5 +7,12 @@ curl -fsSL https://raw.githubusercontent.com/Milo123459/glitter/master/install.s
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
 rustup default stable
+if ! [ -x "$(command -v gh)" ]; then
+    curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+        && chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+        && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+        && apt-get update && export DEBIAN_FRONTEND=noninteractive \
+        && apt-get -y install --no-install-recommends gh
+fi
 sudo apt install fzf
 gh extension install mislav/gh-branch
