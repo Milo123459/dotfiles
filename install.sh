00b53343656076c1de886e85762dd4ee4319628c
Milo <50248166+Milo123459@users.noreply.github.com>
2022-12-22 20:46:04 +0000

    autoinstall npm

--- install.sh
+++ install.sh
@@ -3,9 +3,11 @@ if [ ! -d "$HOME/.config" ]; then mkdir "$HOME/.config"; fi
 cp .bashrc $HOME/.bashrc
 cp starship.toml $HOME/.config/starship.toml
 curl -sS https://starship.rs/install.sh | sh -s -- -y
-curl -fsSL https://raw.githubusercontent.com/Milo123459/glitter/master/install.sh | bash -s -- -y 
+curl -fsSL https://raw.githubusercontent.com/Milo123459/glitter/master/install.sh | bash -s -- -y
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
 rustup default stable
-sudo apt install fzf
-gh extension install mislav/gh-branch
+curl -L https://raw.githubusercontent.com/tj/n/master/bin/n -o n
+sudo bash n lts
+rm n
+sudo npm i -g n
